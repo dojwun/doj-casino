fb801cbec6b8f949f238e8c90e56fa8dca76a4b6
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-10-13 10:16:37 -0700

    added configs

--- casino-walls/config.lua
+++ casino-walls/config.lua
@@ -1,1 +1,15 @@
-config = {}
+Config = {}
+
+Config.PlayCasinoAmbientNoise = true
+Config.SetShowCarOnDisplay = true
+Config.SendWelcomeMail = true
+
+
+Config.SetAnimatedWalls = true
+Config.AnimatedWallNormal = 'CASINO_DIA_PL'
+Config.AnimatedWallWin = 'CASINO_WIN_PL'
+-- WALLS
+-- CASINO_DIA_PL    - Falling Diamonds
+-- CASINO_HLW_PL    - Falling Skulls
+-- CASINO_SNWFLK_PL - Falling Snowflakes
+-- CASINO_WIN_PL    - Falling Confetti
