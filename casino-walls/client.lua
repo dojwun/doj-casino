40b3c1d137a0d7336f5bbfe9738558f612e5f0ac
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-10-15 12:45:00 -0700

    added peds

--- casino-walls/client.lua
+++ casino-walls/client.lua
@@ -218,3 +218,28 @@ function playSomeBackgroundAudioBaby()
 	end)
 end
 
+-- Casino
+local Casino= { 
+  {950.214, 33.151, 70.839 ,"enter",57.052,0xBB0D72F5,"U_F_M_CasinoCash_01"}, --Casino Cashier
+  {955.619, 70.179, 69.433 ,"insidetrack",190.937,579932932,"S_M_Y_Doorman_01"}, --Horse Bets 
+}
+
+Citizen.CreateThread(function()
+  for _,v in pairs(Casino) do
+    RequestModel(GetHashKey(v[7]))
+    while not HasModelLoaded(GetHashKey(v[7])) do
+      Wait(1)
+    end
+
+    RequestAnimDict("mini@strip_club@idles@bouncer@base")
+    while not HasAnimDictLoaded("mini@strip_club@idles@bouncer@base") do
+      Wait(1)
+    end
+    ped =  CreatePed(4, v[6],v[1],v[2],v[3], 3374176, false, true)
+    SetEntityHeading(ped, v[5])
+    FreezeEntityPosition(ped, true)
+    SetEntityInvincible(ped, true)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    TaskPlayAnim(ped,"mini@strip_club@idles@bouncer@base","base", 8.0, 0.0, -1, 1, 0, 0, 0, 0)
+  end
+end)
