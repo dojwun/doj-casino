ed72b0106c10e2fbeb3aef97186e8c39170621da
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-10-28 19:44:05 -0700

    DMCA :p

--- casino-slotmachines/locales/en.lua
+++
@@ -1,6 +0,0 @@
-Locales['en'] = {
-    ['help_play'] = '~INPUT_CELLPHONE_CANCEL~ Stand up\n~INPUT_FRONTEND_RDOWN~ ~INPUT_JUMP~ Spin\n~INPUT_CONTEXT~ Change Camera\nBet: ~y~%s~w~ Chips',
-    ['help_sit'] = 'Press ~INPUT_CONTEXT~ to play slots\nBet: ~y~%s~w~ Chips',
-	['win'] = 'You won',
-	['item'] = 'chips',
-}
