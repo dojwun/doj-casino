85b2f909383c64d34c8c8db0d56150874607d7a6
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-05 20:35:05 -0700

    Update client.lua

--- casino-walls/client.lua
+++ casino-walls/client.lua
@@ -243,3 +243,78 @@ Citizen.CreateThread(function()
     TaskPlayAnim(ped,"mini@strip_club@idles@bouncer@base","base", 8.0, 0.0, -1, 1, 0, 0, 0, 0)
   end
 end)
+
+--=============================================== added
+-- Lucky wheel
+exports['qb-target']:AddCircleZone("LuckyWheel", vector3(949.391, 44.72, 71.638), 2.0, {
+    name="LuckyWheel",
+    heading=160,
+    debugPoly=false,
+    useZ=true,
+    }, {
+        options = {
+            {
+                event = "luckywheel:client:startWheel",
+                icon = "fas fa-sync-alt",
+                label = "Try Your Luck",
+            },
+        },
+    distance = 2.0 
+})
+
+-- Horse Bets
+exports['qb-target']:AddCircleZone("Betting", vector3(956.121,70.185,70.433), 1.0, {
+    name="Betting",
+    heading=160,
+    debugPoly=false,
+    useZ=true,
+}, {
+    options = {
+        {
+            event = "QBCore:client:openInsideTrack",
+            icon = "fas fa-coins",
+            label = "Start Betting",
+        },
+    },
+    distance = 3.0 
+})
+
+-- Casino Shop
+exports['qb-target']:AddTargetModel(`U_F_M_CasinoCash_01`, {
+	options = {
+        { 
+            event = "doj:casinoChipMenu",
+            icon = "fas fa-exchange-alt",
+            label = "Exchange Casino Chips", 
+        },
+        {
+            event = "qb-casino:client:openCasinoChips",
+            icon = "fas fa-coins",
+            label = "Purchase Casino Chips", 
+        },
+        {
+            event = "qb-casino:client:openCasinoMembersips", 
+            icon = "fas fa-id-card",
+            label = "Purchase Casino Memberships", 
+        },
+	},
+	distance = 3.0 
+})
+
+RegisterNetEvent('qb-casino:client:openCasinoMembersips')
+AddEventHandler('qb-casino:client:openCasinoMembersips', function()
+    local ShopItems = {}
+    ShopItems.label = "Diamond Casino Memberships"
+    ShopItems.items = Config.CasinoMemberships
+    ShopItems.slots = #Config.CasinoMemberships
+    TriggerServerEvent("inventory:server:OpenInventory", "shop", "Vendingshop_", ShopItems)
+end)
+
+RegisterNetEvent('qb-casino:client:openCasinoChips')
+AddEventHandler('qb-casino:client:openCasinoChips', function()
+    local ShopItems = {} 
+    ShopItems.label = "Diamond Casino Chips"
+    ShopItems.items = Config.CasinoChips
+    ShopItems.slots = #Config.CasinoChips
+    TriggerServerEvent("inventory:server:OpenInventory", "shop", "Vendingshop_", ShopItems)
+end)
