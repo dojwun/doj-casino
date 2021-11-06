628d288bd1efb4da0582c0d9ba9f7a16aaca94e6
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-05 20:35:27 -0700

    Update config.lua

--- casino-walls/config.lua
+++ casino-walls/config.lua
@@ -17,3 +17,16 @@ Config.SendWelcomeMail = true
 Config.WelcomeMailsender = "The Diamond Casino"
 Config.WelcomeMailsubject ="Welcome!"
 Config.WelcomeMailmessage = "Welcome to The Diamond Casino, We are Open 24/7 & only accept Electronic Transactions"
+
+Config.CasinoMemberships = {
+    [1] = { name = "member", price = 500, amount = 5, info = {}, type = "item", slot = 1 },
+    [2] = { name = "vip",    price = 750, amount = 5, info = {}, type = "item", slot = 2 }
+}
+
+Config.CasinoChips = {
+    [1] = { name = "whitechip", price = 1,   amount = 999, info = {}, type = "item", slot = 1 },
+    [2] = { name = "redchip",   price = 5,   amount = 999, info = {}, type = "item", slot = 2 },
+    [3] = { name = "bluechip",  price = 10,  amount = 999, info = {}, type = "item", slot = 3 },
+    [4] = { name = "blackchip", price = 50,  amount = 999, info = {}, type = "item", slot = 4 },
+    [5] = { name = "goldchip",  price = 100, amount = 999, info = {}, type = "item", slot = 5 }
+}
