1cc219b8fd1da97b745603849cccb2206fe5dfc6
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-14 12:30:32 -0800

    updated with casino chips

--- casino-luckywheel/client.lua
+++ casino-luckywheel/client.lua
@@ -13,48 +13,35 @@ local m2b = GetHashKey('vw_prop_vw_jackpot_on')
 local _wheelPos = Config.WheelPos
 local _isRolling = false
 
-Citizen.CreateThread(function()
-
-	
+CreateThread(function()
 	RequestScriptAudioBank("DLC_VINEWOOD\\CASINO_GENERAL", false)
-
 	local model1 = GetHashKey('vw_prop_vw_luckywheel_02a')
 	local model2 = GetHashKey('vw_prop_vw_luckywheel_01a')
 	local podiumModel = GetHashKey('vw_prop_vw_casino_podium_01a')
-
-	Citizen.CreateThread(function()
-		RequestModel(model1) while not HasModelLoaded(model1) do Citizen.Wait(0) end
-		RequestModel(model2) while not HasModelLoaded(model2) do Citizen.Wait(0) end
-		
-		RequestModel(m1a) while not HasModelLoaded(m1a) do Citizen.Wait(0) end
-		RequestModel(m1b) while not HasModelLoaded(m1b) do Citizen.Wait(0) end
-		RequestModel(m2a) while not HasModelLoaded(m2a) do Citizen.Wait(0) end
-		RequestModel(m2b) while not HasModelLoaded(m2b) do Citizen.Wait(0) end
-		
-		
+	CreateThread(function()
+		RequestModel(model1) while not HasModelLoaded(model1) do Wait(0) end
+		RequestModel(model2) while not HasModelLoaded(model2) do Wait(0) end
+		RequestModel(m1a) while not HasModelLoaded(m1a) do Wait(0) end
+		RequestModel(m1b) while not HasModelLoaded(m1b) do Wait(0) end
+		RequestModel(m2a) while not HasModelLoaded(m2a) do Wait(0) end
+		RequestModel(m2b) while not HasModelLoaded(m2b) do Wait(0) end
 		ClearArea(Config.WheelPos.x, Config.WheelPos.y, Config.WheelPos.z, 5.0, true, false, false, false)
-		
 		_wheel = CreateObject(model1, Config.WheelPos.x, Config.WheelPos.y, Config.WheelPos.z, false, false, true)
 		SetEntityHeading(_wheel, Config.WheelPos.h)
 		SetModelAsNoLongerNeeded(model1)
-
 		_base = CreateObject(model2, Config.WheelPos.x, Config.WheelPos.y, Config.WheelPos.z-0.26, false, false, true)
 		SetEntityHeading(_base, Config.WheelPos.h)
 		SetModelAsNoLongerNeeded(_base)
-
 		_lights1 = CreateObject(m1a, Config.WheelPos.x, Config.WheelPos.y, Config.WheelPos.z+0.35, false, false, true)
 		SetEntityHeading(_lights1, Config.WheelPos.h)
 		SetModelAsNoLongerNeeded(_lights1)
-
 		_lights2 = CreateObject(m1b, Config.WheelPos.x, Config.WheelPos.y, Config.WheelPos.z+0.35, false, false, true)
 		SetEntityVisible(_lights2, false, 0)
 		SetEntityHeading(_lights2, Config.WheelPos.h)
 		SetModelAsNoLongerNeeded(_lights2)
-
 		_arrow1 = CreateObject(m2a, Config.WheelPos.x, Config.WheelPos.y, Config.WheelPos.z+2.5, false, false, true)
 		SetEntityHeading(_arrow1, Config.WheelPos.h)
 		SetModelAsNoLongerNeeded(_arrow1)
-
 		_arrow2 = CreateObject(m2b, Config.WheelPos.x, Config.WheelPos.y, Config.WheelPos.z+2.5, false, false, true)
 		SetEntityVisible(_arrow2, false, 0)
 		SetEntityHeading(_arrow2, Config.WheelPos.h)
@@ -64,16 +51,14 @@ Citizen.CreateThread(function()
 end)
 
 
-Citizen.CreateThread(function()
+CreateThread(function()
 	while true do
         local sleep = 5
         local playerPed = PlayerPedId()
         local inZone = false
         local coords = GetEntityCoords(playerPed)
         local distance = GetDistanceBetweenCoords(coords, _wheelPos.x, _wheelPos.y, _wheelPos.z, true)
-
         if distance < 2.9 and not _isRolling then
-
             if distance < 2.8 and not _isRolling then
                 inZone  = true
 				text = "<b>Diamond Casino Lucky Wheel</b></p> $"..Config.startingPrice.." a spin"
@@ -87,23 +72,19 @@ Citizen.CreateThread(function()
                 exports['textUi']:HideTextUi('hide')
             end
         end
-        Citizen.Wait(sleep)		
+        Wait(sleep)		
 	end
 end)
 
 RegisterNetEvent("luckywheel:client:startWheel")
 AddEventHandler("luckywheel:client:startWheel", function()
-	if Config.CheckMembership then
-		QBCore.Functions.TriggerCallback('QBCore:HasItem', function(HasItem)
-			if HasItem then
-				TriggerServerEvent("luckywheel:getwheel")
-			else
-				QBCore.Functions.Notify('You dont have '..Config.CasinoMembership..' membership to spin', 'error', 3500)
-			end
-		end, Config.CasinoMembership)
-	else
-		TriggerServerEvent("luckywheel:getwheel")
-	end
+	QBCore.Functions.TriggerCallback('QBCore:HasItem', function(HasItem)
+		if HasItem then
+			TriggerServerEvent("luckywheel:getwheel")
+		else
+			QBCore.Functions.Notify('You are not a V.I.P of the casino', 'error', 3500)
+		end
+	end, "casino_vip")
 end)
 
 RegisterNetEvent("luckywheel:syncanim")
@@ -113,7 +94,7 @@ end)
 
 RegisterNetEvent("luckywheel:startroll")
 AddEventHandler("luckywheel:startroll", function(s, index, p)
-	Citizen.Wait(1000)
+	Wait(1000)
 	SetEntityVisible(_lights1, false, 0)
 	SetEntityVisible(_lights2, true, 0)
 	win = (index - 1) * 18 + 0.0
@@ -132,8 +113,6 @@ AddEventHandler("luckywheel:startroll", function(s, index, p)
 			j = j - 2.0
 		elseif i < 150 then
 			j = j - 2.5
-			
-			
 		elseif i > 1060 then
 			j = j - 0.3
 		elseif i > 1030 then
@@ -160,9 +139,9 @@ AddEventHandler("luckywheel:startroll", function(s, index, p)
 		if j < 0 then j = j + 360 end
 		if i == 900 then
 		end
-		Citizen.Wait(0)
+		Wait(0)
 	end
-	Citizen.Wait(300)
+	Wait(300)
 	SetEntityVisible(_arrow1, false, 0)
 	SetEntityVisible(_arrow2, true, 0)
 	local t = true
@@ -184,7 +163,7 @@ AddEventHandler("luckywheel:startroll", function(s, index, p)
 	end
 	
 	for i=1,15,1 do
-		Citizen.Wait(200)
+		Wait(200)
 		SetEntityVisible(_lights1, t, 0)
 		SetEntityVisible(_arrow2, t, 0)
 		t = not t
@@ -196,7 +175,7 @@ AddEventHandler("luckywheel:startroll", function(s, index, p)
 			end
 		end
 	end
-	Citizen.Wait(1000)
+	Wait(1000)
 	SetEntityVisible(_lights1, true, 0)
 	SetEntityVisible(_lights2, false, 0)
 	SetEntityVisible(_arrow1, true, 0)
@@ -214,7 +193,7 @@ function QBCoreRequestAnimDict(animDict, cb)
 		RequestAnimDict(animDict)
 
 		while not HasAnimDictLoaded(animDict) do
-			Citizen.Wait(1)
+			Wait(1)
 		end
 	end
 
@@ -244,23 +223,23 @@ function doRoll(index)
                 if coords.x >= (_movePos.x - 0.01) and coords.x <= (_movePos.x + 0.01) and coords.y >= (_movePos.y - 0.01) and coords.y <= (_movePos.y + 0.01) then
                     _isMoved = true
                 end
-                Citizen.Wait(0)
+                Wait(0)
             end
 			SetEntityHeading(playerPed, GetEntityHeading(_base))
             TaskPlayAnim(playerPed, lib, anim, 8.0, -8.0, -1, 0, 0, false, false, false)
             while IsEntityPlayingAnim(playerPed, lib, anim, 3) do
-				Citizen.Wait(0)
+				Wait(0)
 				DisableAllControlActions(0)
             end
             TaskPlayAnim(playerPed, lib, 'enter_to_armraisedidle', 8.0, -8.0, -1, 0, 0, false, false, false)
             while IsEntityPlayingAnim(playerPed, lib, 'enter_to_armraisedidle', 3) do
-                Citizen.Wait(0)
+                Wait(0)
                 DisableAllControlActions(0)
             end
             TaskPlayAnim(playerPed, lib, 'armraisedidle_to_spinningidle_high', 8.0, -8.0, -1, 0, 0, false, false, false)
-            Citizen.Wait(4800)
+            Wait(4800)
             TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_STRIP_WATCH_STAND", 0, true)
-			Citizen.Wait(4800)
+			Wait(4800)
 			ClearPedTasks(playerPed)
 
         end)
@@ -271,12 +250,11 @@ end
 RegisterNetEvent("dojLuckywheel:winCar")
 AddEventHandler("dojLuckywheel:winCar", function() 
     local coords = { ['x'] = 933.29, ['y'] = -2.82, ['z'] = 78.76, ['h'] = 144.6 }
-
 	QBCore.Functions.SpawnVehicle(Config.VehiclePrize, function(veh)
 		SetVehicleNumberPlateText(veh, "DIAMOND"..tostring(math.random(1000, 9999)))
 		exports['LegacyFuel']:SetFuel(veh, 100.0)
 		SetEntityHeading(veh, coords.h)
-		TaskWarpPedIntoVehicle(GetPlayerPed(-1), veh, -1)
+		TaskWarpPedIntoVehicle(PlayerPedId(), veh, -1)
 		TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(veh))
 		SetVehicleEngineOn(veh, true, true)
 		QBCore.Functions.Notify("YOU WON THE SHOW CAR! Dodge Challenger SRT Demon!", "primary", 3500)
