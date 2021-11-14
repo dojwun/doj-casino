6825c072ebd5d9cb7d4e87a4c6ecc13ea862b96e
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-14 12:29:03 -0800

    updated with casino chips

--- casino-insidetrack/server/server.lua
+++ casino-insidetrack/server/server.lua
@@ -1,56 +1,49 @@
 
 
 
-
-
-
-
-
--- QBCore.Functions.CreateCallback("insidetrack:server:getbalance", function(source, cb) 
---     local Player = QBCore.Functions.GetPlayer(source)
---     local Chips = Player.Functions.GetItemByName("whitechip")
-
---     if Chips ~= nil then 
---         Chips = Chips
---     else
---         return TriggerClientEvent('QBCore:client:closeBets', source)
---     end
--- end)
-
-
 QBCore.Functions.CreateCallback("insidetrack:server:getbalance", function(source, cb)
-    local source = source
-    local Player = QBCore.Functions.GetPlayer(source)
-    local bankBalance = Player.PlayerData.money["bank"]
-    local MinAmount = 100
-
-    if bankBalance >= MinAmount then
-        
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Chips = Player.Functions.GetItemByName("casino_redchip")
+    local minAmount = 100
+    if Chips ~= nil then 
+        if Chips.amount >= minAmount then
+            Chips = Chips 
+        else
+            return TriggerClientEvent('QBCore:client:closeBetsNotEnough', src)
+        end
     else
-        return TriggerClientEvent('QBCore:client:closeBets', source)
+        return TriggerClientEvent('QBCore:client:closeBetsZeroChips', src)
     end
-    
 end)
 
-
-RegisterServerEvent("insidetrack:server:placebet")
-AddEventHandler("insidetrack:server:placebet", function(bet)
-
-    local Player = QBCore.Functions.GetPlayer(source)
-
-    if Player ~= nil then
-        Player.Functions.RemoveMoney("bank", tonumber(bet), "inside-track-bet")
-        TriggerClientEvent('QBCore:Notify', source, "You placed a $"..bet.." bet")
+RegisterServerEvent("insidetrack:server:placebet", function(bet)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Chips = Player.Functions.GetItemByName("casino_redchip")
+    if Chips ~= nil then 
+        if Chips.amount >= bet then
+            Player.Functions.RemoveItem("casino_redchip", bet)
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['casino_redchip'], "remove", bet)
+            TriggerClientEvent('QBCore:Notify', src, "You placed a "..bet.." white casino chips bet")
+        else
+            return TriggerClientEvent('QBCore:client:closeBetsNotEnough', src)
+        end
+    else
+        return TriggerClientEvent('QBCore:client:closeBetsZeroChips', src)
     end
 end) 
 
-RegisterServerEvent("insidetrack:server:winnings")
-AddEventHandler("insidetrack:server:winnings", function(amount)
-    local Player = QBCore.Functions.GetPlayer(source)
+RegisterServerEvent("insidetrack:server:winnings", function(amount)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
     if Player ~= nil then
-        Player.Functions.AddMoney("bank", tonumber(amount), "inside-track-won")
-        TriggerClientEvent('QBCore:Notify', source, "You Won $"..amount.."!")
-
+        if Player.Functions.AddItem('casino_redchip', amount, nil, {["quality"] = 100}) then
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["casino_redchip"], "add", amount)
+            TriggerClientEvent('QBCore:Notify', src, "You Won "..amount.." white casino chips!")
+        else
+            TriggerClientEvent('QBCore:Notify', src, 'You have to much in your pockets', 'error')
+        end
     end
 end) 
 
