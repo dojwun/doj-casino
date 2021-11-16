01a9e2000a9852c1e90677cf7d5d2d85b5765ba6
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-16 13:31:18 -0800

    Update fxmanifest.lua

--- casino-luckywheel/fxmanifest.lua
+++ casino-luckywheel/fxmanifest.lua
@@ -1,14 +1,11 @@
+
+
 fx_version 'adamant'
 game 'gta5'
 description 'Lucky Wheel'
-version '1.5.3'
 
 
 server_script 'server.lua'
-
 client_script 'client.lua'
+shared_script 'config.lua'
 
-
-shared_scripts { 
-	'config.lua',
-}
