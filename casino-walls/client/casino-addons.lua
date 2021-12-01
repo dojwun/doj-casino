e1b84bcf885b5040a76df6b75be908f903889b8c
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-12-01 15:31:19 -0800

    added CasinoEmployeePrompt

--- casino-walls/client/casino-addons.lua
+++ casino-walls/client/casino-addons.lua
@@ -58,17 +58,49 @@ RegisterNetEvent('doj:casinoChipMenu', function()
     })
 end)
 
--- Casino Shop 
-exports['qb-target']:AddTargetModel(`U_F_M_CasinoCash_01`, {
-	options = {
-        { 
-            event = "doj:casinoMainMenu",
-            icon = "fas fa-circle",
-            label = "Speak with Casino Employee", 
-        },
-	},
-	distance = 3.0 
-})
+CreateThread(function()
+    local alreadyEnteredZone = false
+    while true do
+        wait = 5
+        local inZone = false
+        local PlayerPed = PlayerPedId()
+        local PlayerPos = GetEntityCoords(PlayerPed)
+        local dist = #(PlayerPos - vector3(948.739, 34.114, 71.839))
+        if dist <= 2.0 then
+            wait = 5
+            inZone  = true
+            if Config.CasinoEmployeePrompt == 'press' then 
+                text = '<b>Diamond Casino Exchange</b></p>Press [E] to talk to employee'
+                if IsControlJustPressed(0, 38) then -- E
+                    Wait(200)
+                    TriggerEvent('doj:casinoMainMenu') 
+                end
+            elseif Config.CasinoEmployeePrompt == 'peek' then
+                text = '<b>Diamond Casino Exchange</b>'
+                exports['qb-target']:AddTargetModel(`U_F_M_CasinoCash_01`, {
+                    options = {
+                        { 
+                            event = "doj:casinoMainMenu",
+                            icon = "fab fa-speakap",
+                            label = "Speak with Casino Employee", 
+                        },
+                    },
+                    distance = 3.0 
+                })
+            end
+        end
+        if inZone and not alreadyEnteredZone then
+            alreadyEnteredZone = true
+            exports['textUi']:DrawTextUi('show', text)
+        end
+
+        if not inZone and alreadyEnteredZone then
+            alreadyEnteredZone = false
+            exports['textUi']:HideTextUi('hide')
+        end
+        Wait(wait)
+    end
+end)
 
 RegisterNetEvent('qb-casino:client:openCasinoShop', function()
     local ShopItems = {}
