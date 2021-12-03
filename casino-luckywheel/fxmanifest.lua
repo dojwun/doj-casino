9bc2a603d8719d3d127f617da9e7bf7bc5d4941d
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-12-02 21:17:45 -0800

    updated to PolyZone

--- casino-luckywheel/fxmanifest.lua
+++ casino-luckywheel/fxmanifest.lua
@@ -6,6 +6,11 @@ description 'Lucky Wheel'
 
 
 server_script 'server.lua'
-client_script 'client.lua'
-shared_script 'config.lua'
 
+client_scripts {	
+    '@PolyZone/client.lua',
+    '@PolyZone/CircleZone.lua',
+    'client.lua',
+}
+
+shared_script 'config.lua' 
