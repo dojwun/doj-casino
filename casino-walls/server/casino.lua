f613f894cbe4fccadc5269f6065f31cdc630e3bd
mwuhahaa <82911693+mwuhahaa@users.noreply.github.com>
2023-05-03 02:48:46 +0300

    Update casino.lua
    Fixed chip duping money maker problem.

--- casino-walls/server/casino.lua
+++ casino-walls/server/casino.lua
@@ -1,8 +1,14 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+local quantity = 0
 local ItemList = {
     ["casino_whitechip"] = 1,
+	["casino_redchip"] = 1,
+    ["casino_bluechip"] = 1,
+	["casino_blackchip"] = 1,
+	["casino_goldchip"] = 1,
 }
+
 RegisterServerEvent("qb-casino:server:WhiteSell")
 AddEventHandler("qb-casino:server:WhiteSell", function()
     local src = source
@@ -12,81 +18,81 @@ AddEventHandler("qb-casino:server:WhiteSell", function()
     if xItem ~= nil then
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
-                if ItemList[Player.PlayerData.items[k].name] ~= nil then 
-                    price = price * (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                if ItemList[Player.PlayerData.items[k].name] ~= nil then
+                    quantity = quantity + Player.PlayerData.items[k].amount
                     Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
-                    Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
-                    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_whitechip'], "remove", Player.PlayerData.items[k].amount)
-                    TriggerClientEvent('QBCore:Notify', src, "You sold "..Player.PlayerData.items[k].amount.." White chips for $"..price)
-                    TriggerClientEvent("doj:casinoChipMenu", src)
                 end
             end
         end
+        price = price * quantity
+        Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_whitechip'], "remove", quantity)
+        TriggerClientEvent('QBCore:Notify', src, "You sold "..quantity.." white chips for "..price.."€")
+        TriggerEvent('qb-log:server:CreateLog', 'soldchips', 'Sale Money | '..Player.PlayerData.name, 'default', quantity.." white chips for "..price.."€")
+        TriggerClientEvent("doj:casinoChipMenu", src)
+        quantity = 0
     else
-        TriggerClientEvent('QBCore:Notify', src, "You dont have any white casino chips...", "error")
+        TriggerClientEvent('QBCore:Notify', src, "You don't have any white chips to sell.", "error")
         TriggerClientEvent("doj:casinoChipMenu", src)
     end
 end)
 
-local ItemList = {
-    ["casino_redchip"] = 1,
-}
 RegisterServerEvent("qb-casino:server:RedSell")
 AddEventHandler("qb-casino:server:RedSell", function()
     local src = source
     local price = Config.redChipPrice
     local Player = QBCore.Functions.GetPlayer(src)
     local xItem = Player.Functions.GetItemByName("casino_redchip")
-    if xItem ~= nil then 
+    if xItem ~= nil then
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
-                if ItemList[Player.PlayerData.items[k].name] ~= nil then 
-                    price = price * (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                if ItemList[Player.PlayerData.items[k].name] ~= nil then
+                    quantity = quantity + Player.PlayerData.items[k].amount
                     Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
-                    Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
-                    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_redchip'], "remove", Player.PlayerData.items[k].amount)
-                    TriggerClientEvent('QBCore:Notify', src, "You sold "..Player.PlayerData.items[k].amount.." Red chips for $"..price)
-                    TriggerClientEvent("doj:casinoChipMenu", src)
                 end
             end
         end
+        price = price * quantity
+        Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_redchip'], "remove", quantity)
+        TriggerClientEvent('QBCore:Notify', src, "You sold "..quantity.." Red chips for $"..price)
+        TriggerEvent('qb-log:server:CreateLog', 'guedesteste', 'Dinheiro Venda | '..Player.PlayerData.name, 'default', quantity.." fichas vermelhas por "..price.."€")
+        TriggerClientEvent("doj:casinoChipMenu", src)
+        quantity = 0
     else
         TriggerClientEvent('QBCore:Notify', src, "You dont have any red casino chips...", "error")
         TriggerClientEvent("doj:casinoChipMenu", src)
     end
 end)
 
-local ItemList = {
-    ["casino_bluechip"] = 1,
-}
 RegisterServerEvent("qb-casino:server:BlueSell")
 AddEventHandler("qb-casino:server:BlueSell", function()
     local src = source
     local price = Config.blueChipPrice
     local Player = QBCore.Functions.GetPlayer(src)
     local xItem = Player.Functions.GetItemByName("casino_bluechip")
-    if xItem ~= nil then
+    if xItem ~= nil then 
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
-                if ItemList[Player.PlayerData.items[k].name] ~= nil then 
-                    price = price * (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                if ItemList[Player.PlayerData.items[k].name] ~= nil then
+                    quantity = quantity + Player.PlayerData.items[k].amount
                     Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
-                    Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
-                    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_bluechip'], "remove", Player.PlayerData.items[k].amount)
-                    TriggerClientEvent('QBCore:Notify', src, "You sold "..Player.PlayerData.items[k].amount.." Blue chips for $"..price)
-                    TriggerClientEvent("doj:casinoChipMenu", src)
                 end
             end
         end
+        price = price * quantity
+        Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_bluechip'], "remove", quantity)
+        TriggerClientEvent('QBCore:Notify', src, "You sold "..quantity.." Blue chips for $"..price)
+        TriggerEvent('qb-log:server:CreateLog', 'guedesteste', 'Dinheiro Venda | '..Player.PlayerData.name, 'default', quantity.." fichas azuis por "..price.."€")
+        TriggerClientEvent("doj:casinoChipMenu", src)
+        quantity = 0
     else
         TriggerClientEvent('QBCore:Notify', src, "You dont have any blue casino chips...", "error")
         TriggerClientEvent("doj:casinoChipMenu", src)
     end
 end)
 
-local ItemList = {
-    ["casino_blackchip"] = 1,
-}
 RegisterServerEvent("qb-casino:server:BlackSell")
 AddEventHandler("qb-casino:server:BlackSell", function()
     local src = source
@@ -96,25 +102,25 @@ AddEventHandler("qb-casino:server:BlackSell", function()
     if xItem ~= nil then
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
-                if ItemList[Player.PlayerData.items[k].name] ~= nil then 
-                    price = price * (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                if ItemList[Player.PlayerData.items[k].name] ~= nil then
+                    quantity = quantity + Player.PlayerData.items[k].amount
                     Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
-                    Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
-                    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_blackchip'], "remove", Player.PlayerData.items[k].amount)
-                    TriggerClientEvent('QBCore:Notify', src, "You sold "..Player.PlayerData.items[k].amount.." Black chips for $"..price)
-                    TriggerClientEvent("doj:casinoChipMenu", src)
                 end
             end
         end
+        price = price * quantity
+        Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_blackchip'], "remove", quantity)
+        TriggerClientEvent('QBCore:Notify', src, "You sold "..quantity.." black chips for $"..price)
+        TriggerEvent('qb-log:server:CreateLog', 'blackchip-sell', 'Money Earned | '..Player.PlayerData.name, 'default', quantity.." black chips for $"..price)
+        TriggerClientEvent("doj:casinoChipMenu", src)
+        quantity = 0
     else
         TriggerClientEvent('QBCore:Notify', src, "You dont have any black casino chips...", "error")
         TriggerClientEvent("doj:casinoChipMenu", src)
     end
 end)
 
-local ItemList = {
-    ["casino_goldchip"] = 1,
-}
 RegisterServerEvent("qb-casino:server:GoldSell")
 AddEventHandler("qb-casino:server:GoldSell", function()
     local src = source
@@ -122,36 +128,24 @@ AddEventHandler("qb-casino:server:GoldSell", function()
     local Player = QBCore.Functions.GetPlayer(src)
     local xItem = Player.Functions.GetItemByName("casino_goldchip")
     if xItem ~= nil then
+        local quantity = 0
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
                 if ItemList[Player.PlayerData.items[k].name] ~= nil then 
-                    price = price * (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                    quantity = quantity + Player.PlayerData.items[k].amount
                     Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
-                    Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
-                    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_goldchip'], "remove", Player.PlayerData.items[k].amount)
-                    TriggerClientEvent('QBCore:Notify', src, "You sold "..Player.PlayerData.items[k].amount.." Gold chips for $"..price)
-                    TriggerClientEvent("doj:casinoChipMenu", src)
                 end
             end
         end
+        price = price * quantity
+        Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_goldchip'], "remove", quantity)
+        TriggerClientEvent('QBCore:Notify', src, "You sold "..quantity.." Gold chips for $"..price)
+        TriggerEvent('qb-log:server:CreateLog', 'guedesteste', 'Dinheiro Venda | '..Player.PlayerData.name, 'default', quantity.." fichas de ouro por "..price.."€")
+        TriggerClientEvent("doj:casinoChipMenu", src)
+		quantity = 0
     else
         TriggerClientEvent('QBCore:Notify', src, "You dont have any gold casino chips...", "error") 
         TriggerClientEvent("doj:casinoChipMenu", src)
     end
 end)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
