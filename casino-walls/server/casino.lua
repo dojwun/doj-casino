0afbd5bdad44aa1f227a3fef5b44010c0897d727
/dojwun <79622405+dojwun@users.noreply.github.com>
2023-07-09 18:49:39 -0700

    Fixed chip duping money maker problem. -Credit mwuhahaa

--- casino-walls/server/casino.lua
+++ casino-walls/server/casino.lua
@@ -1,32 +1,39 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-
+local quantity = 0
 local ItemList = {
     ["casino_goldchip"] = 1,
 }
-RegisterServerEvent("qb-casino:server:GoldSell", function()
+
+RegisterServerEvent("qb-casino:server:GoldSell")
+AddEventHandler("qb-casino:server:GoldSell", function()
     local src = source
-    local price = Config.casinoChipPrice 
+    local price = Config.casinoChipPrice
     local Player = QBCore.Functions.GetPlayer(src)
     local xItem = Player.Functions.GetItemByName("casino_goldchip")
     if xItem ~= nil then
+        local quantity = 0
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
                 if ItemList[Player.PlayerData.items[k].name] ~= nil then 
-                    price = price * (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                    quantity = quantity + Player.PlayerData.items[k].amount
                     Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
-                    Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
-                    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_goldchip'], "remove", Player.PlayerData.items[k].amount)
-                    TriggerClientEvent('QBCore:Notify', src, "You sold "..Player.PlayerData.items[k].amount.." Gold chips for $"..price)
-                    TriggerClientEvent("doj:casinoChipMenu", src)
                 end
             end
         end
+        price = price * quantity
+        Player.Functions.AddMoney(Config.payment, price, "sold-casino-chips")
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['casino_goldchip'], "remove", quantity)
+        TriggerClientEvent('QBCore:Notify', src, "You sold "..quantity.." Gold chips for $"..price)
+        TriggerEvent('qb-log:server:CreateLog', 'guedesteste', 'Dinheiro Venda | '..Player.PlayerData.name, 'default', quantity.." fichas de ouro por "..price.."€")
+        TriggerClientEvent("doj:casinoChipMenu", src)
+		quantity = 0
     else
         TriggerClientEvent('QBCore:Notify', src, "You dont have any gold casino chips...", "error") 
         TriggerClientEvent("doj:casinoChipMenu", src)
     end
 end)
+
 
 RegisterNetEvent("doj:server:purchaseMembership", function()
     local src = source
