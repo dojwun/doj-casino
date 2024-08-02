2f370d0b7446b38f6aa7faac172d9cfced54fc9e
/dojwun <79622405+dojwun@users.noreply.github.com>
2024-08-02 16:30:55 -0700

    Update fxmanifest.lua

--- casino-slots/fxmanifest.lua
+++ casino-slots/fxmanifest.lua
@@ -6,8 +6,16 @@ description 'A casino resource for which you do not have to sell a kidney for'
 version '1.0.0'
 repository 'https://github.com/Disabled-Coding/dc-casino'
 
-shared_script 'config.lua'
-client_script 'client.lua'
-server_script 'server.lua'
+shared_scripts {
+    '@ox_lib/init.lua',
+    'configs/*.lua'
+}
+client_scripts {
+    'client/slots.lua',
+}
+server_scripts {
+    'server/slots.lua',
+}
 
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
